917bc0796f9bdfa49dc42b0f355e92ab88613cfc
24HP4679 24HP4679 <24HP4679@admins-iMac.local>
2025-03-11 02:56:31 -0700

    home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono,  Oswald, Playfair_Display} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const geistPlayfairDisplay = Playfair_Display({
+  variable: "--font-geist-playfair_display",
+  subsets :["latin"]
+})
+
+const geistOswald = Oswald({
+  variable: "--font-geist-oswald",
+  subsets: ["latin"]
+})
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -25,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${geistOswald.variable} ${geistPlayfairDisplay.variable} antialiased`}
       >
         {children}
       </body>
