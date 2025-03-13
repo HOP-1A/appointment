c40357f51165a2132409eb003440cfb6f57935eb
24HP6473 24HP6473 <24HP6473@admins-iMac.local>
2025-03-13 17:48:26 +0800

    mm

--- src/app/admin/calendar/page.tsx
+++ src/app/admin/calendar/page.tsx
@@ -1,9 +1,7 @@
 "use client";
-import { useEffect, useState } from "react";
 
-import { useCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
+import { useNextCalendarApp, ScheduleXCalendar } from "@schedule-x/react";
 import {
-  CalendarApp,
   createViewDay,
   createViewMonthAgenda,
   createViewMonthGrid,
@@ -12,17 +10,25 @@ import {
 import { createEventsServicePlugin } from "@schedule-x/events-service";
 
 import "@schedule-x/theme-default/dist/index.css";
+import { useEffect, useState } from "react";
 
 function CalendarApp() {
   const eventsService = useState(() => createEventsServicePlugin())[0];
-
-  const calendar = useCalendarApp({
+  const getFunction = async () => {
+    const bookedTime = await fetch("/api/weekly-appointment");
+    console.log(bookedTime);
+  };
+  useEffect(() => {
+    getFunction();
+  }, []);
+  const calendar = useNextCalendarApp({
     views: [
       createViewDay(),
       createViewWeek(),
       createViewMonthGrid(),
       createViewMonthAgenda(),
     ],
+
     events: [
       {
         id: "1",
@@ -30,17 +36,25 @@ function CalendarApp() {
         start: "2023-12-16",
         end: "2023-12-16",
       },
+      {
+        id: "1",
+        title: "GG",
+        start: "2025-03-16 03:00",
+        end: "2025-03-16 04:00",
+      },
     ],
     plugins: [eventsService],
+    callbacks: {
+      onRender: () => {
+        // get all events
+        eventsService.getAll();
+      },
+    },
   });
-
-  useEffect(() => {
-    eventsService.getAll();
-  }, []);
 
   return (
     <div>
-      <ScheduleXCalendar calendarApp={calendar as CalendarApp | undefined} />
+      <ScheduleXCalendar calendarApp={calendar} />
     </div>
   );
 }
