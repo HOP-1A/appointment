7bb49fa60a267e5f6119a08f69d79cac1256cd10
24HP7482 24HP7482 <24HP7482@admins-iMac.local>
2025-03-18 17:14:18 +0800

    appointments zasah

--- src/app/admin/calendar/Appointments.tsx
+++ src/app/admin/calendar/Appointments.tsx
@@ -6,29 +6,34 @@ const Appointments = ({ events }: { events: Appointment[] }) => {
   const date = format(new Date(), "yyyy-MM-dd");
   const week = getWeek(date);
   return (
-    <div className="border pb-4 rounded-md w-[570px] h-fit">
-      <div className="pt-7 pb-2 font-bold pl-5">Weekly appointments</div>
-      <div className=" pb-5 pl-5 text-gray-500 ">{date.toString()}</div>
+    <div className="border pb-4 pr-4 rounded-md w-[500px] h-fit">
+      <div className="flex justify-center items-center">
+        <div className="pt-7 pb-2 font-bold pl-5">📍Weekly appointments</div>
+        <div className=" pt-10 pb-5 pl-5 text-gray-500 ">{date.toString()}</div>
+      </div>
+
       <div className="pl-5">
-        <div className="border rounded-md w-[450px] pl-3">
+        <div>
           {events
             ?.filter((user) => getWeek(user.startDate) === week)
             .map((user, index) => (
-              <div key={index}>
-                <div className="flex justify-between items-center p-2">
-                  <div className="flex space-x-3 font-bold">
-                    <div>{user.firstName}</div>
-                    <div>{user.lastName}</div>
-                  </div>
-                  <div className="text-gray-500">
-                    {format(user.startDate, "yyyy-MM-dd hh:mm")}
+              <div key={index} className="pt-3">
+                <div className="border rounded-md w-[450px] pl-3">
+                  <div className="flex justify-between items-center p-2">
+                    <div className="flex space-x-3 font-bold">
+                      <div>{user.firstName}</div>
+                      <div>{user.lastName}</div>
+                    </div>
+                    <div className="text-gray-500">
+                      {format(user.startDate, "yyyy-MM-dd hh:mm")}
+                    </div>
                   </div>
 
-                  <div className="text-center">
+                  {/* <div className="text-center">
                     <div className=" flex justify-center items-center text-green-800 font-bold bg-green-300 border-green-950 rounded-xl text-sm pb-4 h-4 w-20 pt-4 ">
                       {user.reason}
                     </div>
-                  </div>
+                  </div> */}
                 </div>
               </div>
             ))}
