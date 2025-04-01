01f490b4d4408e8152e15d8e5cf76ee580e91c86
24HP1905 24HP1905 <24HP1905@admins-iMac.local>
2025-04-01 17:43:10 +0800

    time booking

--- src/app/api/time-book/route.ts
+++ src/app/api/time-book/route.ts
@@ -4,15 +4,30 @@ import { NextResponse } from "next/server";
 export const POST = async (req: Request) => {
   try {
     const body = await req.json();
-
     const bookedTime = await prisma.bookTime.findFirst({
       where: {
-        userId: body.userId,
-        startDate: body.startDate,
+        OR: [
+          {
+            startDate: {
+              gte: body.startDate,
+              lt: body.endDate,
+            },
+          },
+          {
+            endDate: {
+              gt: body.startDate,
+              lte: body.endDate,
+            },
+          },
+          {
+            AND: [
+              { startDate: { lte: body.startDate } },
+              { endDate: { gte: body.endDate } },
+            ],
+          },
+        ],
       },
     });
-
-    console.log(bookedTime);
 
     if (bookedTime) {
       return NextResponse.json(
@@ -23,20 +38,20 @@ export const POST = async (req: Request) => {
           status: 401,
         }
       );
+    } else {
+      const bookTime = await prisma.bookTime.create({
+        data: {
+          userId: body.userId,
+          reason: body.reason,
+          startDate: body.startDate,
+          endDate: body.endDate,
+          firstName: body.firstName,
+          lastName: body.lastName,
+          phoneNumber: body.phoneNumber,
+        },
+      });
+      return NextResponse.json(bookTime);
     }
-
-    const bookTime = await prisma.bookTime.create({
-      data: {
-        userId: body.userId,
-        reason: body.reason,
-        startDate: body.startDate,
-        endDate: body.endDate,
-        firstName: body.firstName,
-        lastName: body.lastName,
-        phoneNumber: body.phoneNumber,
-      },
-    });
-    return NextResponse.json(bookTime);
   } catch (error) {
     console.error("error!", error);
     return NextResponse.json(
